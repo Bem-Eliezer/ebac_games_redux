eebffb02796cdb25262226805066c1ac90132401
Bem-Eliezer <eliezer.santos1010@gmail.com>
2024-09-15 20:39:47 -0300

    ultimo commit

---
+++ src/containers/Produtos/__tests__/Produtos.tests.tsx
@@ -0,0 +1,72 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { screen, waitFor } from '@testing-library/react'
+
+import Produtos from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+const mocks = [
+  {
+    id: 1,
+    categoria: 'RPG',
+    imagem: '',
+    plataformas: ['Windows'],
+    preco: 150.9,
+    precoAntigo: 199.9,
+    titulo: 'Elden Ring'
+  },
+  {
+    id: 2,
+    categoria: 'RPG',
+    imagem: '',
+    plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+    preco: 199.9,
+    precoAntigo: 299.9,
+    titulo: 'Hogwarts Legacy'
+  },
+  {
+    id: 3,
+    categoria: 'aventura',
+    imagem: '',
+    plataformas: ['Windows'],
+    preco: 150,
+    precoAntigo: 350,
+    titulo: 'Dora aventureira'
+  },
+  {
+    id: 4,
+    categoria: 'suspense',
+    imagem: '',
+    plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+    preco: 200,
+    precoAntigo: 800,
+    titulo: 'Batman'
+  }
+]
+
+const server = setupServer(
+  rest.get(
+    'http://localhost:4000/produtos',
+    (requisicao, resposta, contexto) => {
+      return resposta(contexto.json(mocks))
+    }
+  )
+)
+
+describe('Testes para a area de produtos', () => {
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
+
+  test('Deve renderizar corretamente com texto de carregamento', () => {
+    renderizaComProvider(<Produtos />)
+    expect(screen.getByText('Carregando...')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar corretamente com a listagem de jogos', async () => {
+    renderizaComProvider(<Produtos />)
+    await waitFor(() => {
+      expect(screen.getByText('Batman')).toBeInTheDocument()
+    })
+  })
+})
